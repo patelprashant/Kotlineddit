6d3cb88989877ddb0843a6928fb88c194cc23392
Prashant <prashant.patel77@gmail.com>
2017-05-31 23:01:45 +1000

    Added fragement + Removed unused elements.

--- app/src/main/java/com/example/kotlineddit/MainActivity.kt
+++ app/src/main/java/com/example/kotlineddit/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.example.kotlineddit
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
-import android.support.v4.app.FragmentManager
-import android.support.v4.app.Fragment
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
