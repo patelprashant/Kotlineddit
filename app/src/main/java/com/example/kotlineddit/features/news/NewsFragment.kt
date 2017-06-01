372784e304a82cb3c7355663106bc50c48cc8d30
Prashant <prashant.patel77@gmail.com>
2017-06-01 22:44:19 +1000

    Added Android extension + Delegated properties - lazy

--- app/src/main/java/com/example/kotlineddit/features/news/NewsFragment.kt
+++ app/src/main/java/com/example/kotlineddit/features/news/NewsFragment.kt
@@ -3,24 +3,26 @@ package com.example.kotlineddit.features.news
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.kotlineddit.R
 import com.example.kotlineddit.commons.inflate
+import kotlinx.android.synthetic.main.news_fragment.*
 
 
 class NewsFragment: Fragment() {
-    private var newsList: RecyclerView? = null
+    private val newsList by lazy {
+        news_list
+    }
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = container?.inflate(R.layout.news_fragment)
+        return container?.inflate(R.layout.news_fragment)
+    }
 
-        newsList = view?.findViewById(R.id.news_list) as RecyclerView?
-        newsList?.setHasFixedSize(true)
-        newsList?.layoutManager = LinearLayoutManager(context)
-
-        return view
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        newsList.setHasFixedSize(true)
+        newsList.layoutManager = LinearLayoutManager(context)
     }
 
 }
